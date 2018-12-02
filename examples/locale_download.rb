feb959d23e57fc907cab3641b4f14f302cba4cc3
Sebastian Nepote <sebastian.nepote@helpling.com>
2018-12-02 20:18:07 +0100

    Adds list filter by name and locale download

--- examples/locale_download.rb
+++ examples/locale_download.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-# downloads complete locales of all the projects updated after 3 days ago
+### Downloads and print all translations from the first 25 the projects and first 25 locales
 
+require 'time'
+require 'benchmark'
 require_relative '../lib/phraseapp-rest'
 
 Phraseapp::Rest.configure do |config|
   config.token = ENV['PHRASEAPP_API_KEY']
 end
-
-three_days_ago = Time.now - 60 * 60 * 24 * 3
-last_update = three_days_ago
 api = Phraseapp::Rest::Factory.api
 
-projects = Phraseapp::Rest::Resource::Project.new(client: api)
-projects.list(updated_after: last_update).each do |project|
-  puts "[project] #{project[:id]} #{project[:updated_at]} #{project[:name]}"
-  locales = Phraseapp::Rest::Resource::Locale.new(client: api, project_id: project[:id])
-  locales.list(updated_after: last_update).each do |locale|
-    puts "  [locale] #{locale[:id]} #{locale[:updated_at]} #{locale[:name]}"
-    puts locales.download(id: locale[:id])
+benchmark = Benchmark.measure do
+  Phraseapp::Rest::Resource::Project.new(client: api).list.each do |project|
+    puts "[project] #{project[:id]} #{project[:updated_at]} #{project[:name]}"
+    Phraseapp::Rest::Resource::Locale.new(client: api, project_id: project[:id]).list.each do |locale|
+      puts "  [locale] #{locale[:id]} #{locale[:updated_at]} #{locale[:name]}"
+      puts locales.download(id: locale[:id])
+    end
   end
 end
+puts benchmark
